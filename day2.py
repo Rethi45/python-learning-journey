db3ed4628529e3de766ff486539b043c84e226c2
Rethiga Srinivasan <rethigasrinivasan507@gmail.com>
2026-07-18 21:31:59 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,21 @@
+# Day 2 - Python Variables and Data Types
+
+name = "Riya"
+branch = "AI & Data Science"
+skill = "Python"
+
+age = 19
+day = 2
+
+cgpa = 8.32
+
+learning_python = True
+
+
+print("Name:", name)
+print("Branch:", branch)
+print("Current Skill:", skill)
+print("Age:", age)
+print("Learning Day:", day)
+print("CGPA:", cgpa)
+print("Python Journey Started:", learning_python)
