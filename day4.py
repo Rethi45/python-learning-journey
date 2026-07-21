7ca4fdad6c0e7b2d735455a2b94c41f49a1e2ab8
Rethiga Srinivasan <rethigasrinivasan507@gmail.com>
2026-07-21 06:19:19 +0530

    Add files via upload
    Day 4 - Arithmetic Operators

---
+++ day4.py
@@ -0,0 +1,12 @@
+# Day 4 - Arithmetic Operators
+
+num1 = int(input("Enter First Number: "))
+num2 = int(input("Enter Second Number: "))
+
+print("Addition :", num1 + num2)
+print("Subtraction :", num1 - num2)
+print("Multiplication :", num1 * num2)
+print("Division :", num1 / num2)
+print("Floor Division :", num1 // num2)
+print("Modulus :", num1 % num2)
+print("Power :", num1 ** num2)
