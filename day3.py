24590c3843ed263e14e3c1483aa0d91e04899108
Rethiga Srinivasan <rethigasrinivasan507@gmail.com>
2026-07-21 06:18:41 +0530

    Add files via upload
    Day 3 - Data Types and Type Casting

---
+++ day3.py
@@ -0,0 +1,10 @@
+# Day 3 - Data Types and Type Casting
+
+name = input("Enter your Name: ")
+age = int(input("Enter your Age: "))
+cgpa = float(input("Enter your CGPA: "))
+
+print("\n----- STUDENT DETAILS -----")
+print("Name :", name)
+print("Age :", age)
+print("CGPA :", cgpa)
